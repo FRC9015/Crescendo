727998185bca3fcd4895d1cdc91a146f6f44b351
Omkaarcodes <145941839+Omkaarcodes@users.noreply.github.com>
2024-01-26 16:55:18 -0500

    Initialized IntakeSubsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Swerve.SwerveSubsystem;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    public static final SwerveSubsystem swerveDrive = new SwerveSubsystem();
+
+	public static final IMU gyro = new IMU();
+
+	private final LimelightInterface limelight = new LimelightInterface();
+
+    public boolean intakePivotState() {
+        //this does nothing, needs to be changed.
+        return false;
+    }
+    public Command intakeNote(){
+        //this does nothing, needs to be changed.
+        return new InstantCommand();
+    }
+
+    public Command returnToCollectionPosition(){
+        //this does nothing, needs to be changed.
+        return new InstantCommand();
+    }
+
+    public Command returnToHandoffPosition() {
+        //this does nothing, needs to be changed.
+        return new InstantCommand();
+    }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+  
+    @Override
+    public void simulationPeriodic() {
+      // This method will be called once per scheduler run during simulation
+    }
+
+    }
+
+
