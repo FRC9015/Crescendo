9839fefb1b3622c5cfe30c4911b769d684d2d5f8
Raunak <69701767+REVSIX@users.noreply.github.com>
2024-02-20 08:23:55 -0500

    Used RobotContainer to Fetch Subsystem Instances

--- src/main/java/frc/robot/commands/ScoreAmp.java
+++ src/main/java/frc/robot/commands/ScoreAmp.java
@@ -2,20 +2,15 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.subsystems.PivotSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
+import static frc.robot.RobotContainer.PIVOT;
+import static frc.robot.RobotContainer.SHOOTER;
 
 public class ScoreAmp extends Command {
-    private ShooterSubsystem Shooter;
-    private PivotSubsystem Pivot;
-
     @Override
     public void execute(){
-        Pivot.setGoal(180); //TODO Needs to be tuned this is a placeholder value
-        Pivot.enable();
+        PIVOT.setGoal(180); //TODO Needs to be tuned this is a placeholder value
+        PIVOT.enable();
         new WaitCommand(0.6); //TODO needs to be tuned based on how long the pivot takes to get to goal
-        Shooter.shootNoteToAmp();
-
+        SHOOTER.shootNoteToAmp();
     }
-
 }
