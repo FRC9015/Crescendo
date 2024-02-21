c7ee2e4be6e7bf53f79ae5ff16d8d83af27ebe39
Raunak <69701767+REVSIX@users.noreply.github.com>
2024-02-20 19:20:41 -0500

    Fixed Handoff Command Structure

--- src/main/java/frc/robot/commands/Handoff.java
+++ src/main/java/frc/robot/commands/Handoff.java
@@ -1,21 +1,20 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import static frc.robot.RobotContainer.INTAKE;
-import static frc.robot.RobotContainer.PIVOT;
-import static frc.robot.RobotContainer.SHOOTER;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 
 
-public class Handoff extends Command {
-    @Override
-    public void execute(){
-        PIVOT.setGoal(0); //TODO Make this the Encoder's offset
-        PIVOT.enable();
-        new WaitCommand(0.6); //Tune value
-        INTAKE.intakeNote();
-        SHOOTER.ampIntake();
+public class Handoff extends ParallelCommandGroup {
+//        PIVOT.setGoal(0); //TODO Make this the Encoder's offset
+//        PIVOT.enable();
+//        new WaitCommand(0.6); //Tune value
+    public Handoff(IntakeSubsystem intake, ShooterSubsystem shooter){
+        addCommands(
+                intake.intakeNote(),
+                shooter.ampIntake()
+        );
     }
-
 }
