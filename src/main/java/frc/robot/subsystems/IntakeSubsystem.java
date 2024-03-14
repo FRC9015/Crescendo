0ab46d063e46ad19cb2a7a5c29088b0ffc163181
Arya <playzoreo10@gmail.com>
2024-03-14 19:19:50 -0400

    Fixing Merge Conflicts

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,7 +6,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.DigitalOutput;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -37,9 +39,9 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public Command autoIntakeNote(){
         return new SequentialCommandGroup(
-                new InstantCommand(this::setIntakeMotorSpeeds),
+                this.runOnce(this::setIntakeMotorSpeeds),
                 new WaitCommand(1),
-                new InstantCommand(this::stopIntakeMotors));
+                this.runOnce(this::stopIntakeMotors));
     }
 
     public Command outtakeNote(){
@@ -51,9 +53,9 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public Command autoOuttakeNote(){
         return new SequentialCommandGroup(
-                new InstantCommand(this::setReverseIntakeMotorSpeeds),
+                this.runOnce(this::setReverseIntakeMotorSpeeds),
                 new WaitCommand(3),
-                new InstantCommand(this::stopIntakeMotors));
+                this.runOnce(this::stopIntakeMotors));
     }
 
     public Command stopIntake(){
