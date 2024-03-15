dcdf29c36d562900c8b9034f842ae48a0dba55ac
Matthew <goofygooberguy111@gmail.com>
2024-03-15 16:17:48 -0400

    Cleaned up code to use Java Color class.

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -4,28 +4,23 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdle.VBatOutputMode;
 import com.ctre.phoenix.led.CANdleConfiguration;
 import com.ctre.phoenix.led.RainbowAnimation;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Constants;
+
+import java.awt.Color;
+
 import static frc.robot.RobotContainer.INTAKE;
 
 public class LEDSubsystem extends SubsystemBase {
   /** Creates a new LED. */
-  CANdle candle = new CANdle(Constants.LEDConstants.CANDleID1);
+  private CANdle candle = new CANdle(Constants.LEDConstants.CANDleID1);
 
-    //for reference; actually type in the numbers into the setLEDs method when giving the Leds a color.
-    private static final Color orange = new Color(255, 25, 0);
-    private static final Color purple = new Color(184, 0, 185);
-    private static final Color yellow = new Color(242, 60, 0);
-    private static final Color green = new Color(56, 209, 0);
-    private static final Color blue = new Color(8, 32, 255);
-    private static final Color red = new Color(255, 0, 0);
-    private static final Color white = new Color(255, 255, 255);
     public LEDSubsystem() {
             CANdleConfiguration candleConfiguration = new CANdleConfiguration();
             candleConfiguration.statusLedOffWhenActive = true;
@@ -36,39 +31,13 @@ public class LEDSubsystem extends SubsystemBase {
             candle.configAllSettings(candleConfiguration, 100);
 
             setDefaultCommand(new InstantCommand(this::setSampleAnimation));
-
     }
     public void setBrightness(double percent) {
         candle.configBrightnessScalar(percent, 100);
     }
 
-    public void setOrange(){
-        candle.setLEDs(orange.red, orange.green, orange.blue);
-        candle.modulateVBatOutput(0.9);
-    }
-    public void setBlue(){
-        candle.setLEDs(blue.red,blue.green,blue.blue);
-        candle.modulateVBatOutput(0.9);
-    }
-    public void setGreen(){
-        candle.setLEDs(green.red,green.green,green.blue);
-        candle.modulateVBatOutput(0.9);
-    }
-    public void setPurple(){
-        candle.setLEDs(purple.red, purple.green, purple.blue);
-        candle.modulateVBatOutput(0.9);
-    }
-    public void setYellow(){
-        candle.setLEDs(yellow.red, yellow.green, yellow.blue);
-        candle.modulateVBatOutput(0.9);
-    }
-    public void setRed(){
-        candle.setLEDs(red.red,red.green,red.blue);
-        candle.modulateVBatOutput(0.9);
-    }
-
-    public void setWhite(){
-        candle.setLEDs(white.red, white.green, white.blue);
+    public void setColor(Color color){
+        candle.setLEDs(color.getRed(), color.getGreen(), color.getBlue());
         candle.modulateVBatOutput(0.9);
     }
     public void setSampleAnimation() {
@@ -82,25 +51,13 @@ public class LEDSubsystem extends SubsystemBase {
 
     public void indicateNote() {
         if (INTAKE.getNoteStatus()) {
-            setOrange();
+            setColor(Color.orange);
         }
     }
 
     public void indicateEmpty() {
         if (INTAKE.getNoteStatus()){
-            setBlue();
-        }
-    }
-
-    public static class Color {
-        public int red;
-        public int green;
-        public int blue;
-
-        public Color(int red, int green, int blue) {
-            this.red = red;
-            this.green = green;
-            this.blue = blue;
+            setColor(Color.blue);
         }
     }
 }
