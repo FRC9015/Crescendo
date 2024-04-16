5255d98b608a32cd6572f1141b0234d300ad1f2b
waffleattack <tbaeyens411@gmail.com>
2024-04-16 10:27:46 -0400

    fixed AutoDrive to make units consistent and fix spinning

--- src/main/java/frc/robot/Commands/AutoDrive.java
+++ src/main/java/frc/robot/Commands/AutoDrive.java
@@ -1,28 +1,21 @@
 package frc.robot.Commands;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import org.littletonrobotics.junction.Logger;
+
 import static frc.robot.RobotContainer.LIMELIGHT_INTERFACE;
 import static frc.robot.RobotContainer.SWERVE;
 import static java.lang.Math.abs;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
 
-import org.littletonrobotics.junction.Logger;
+public class AutoDrive extends Command {
+    PIDController limelightPID = new PIDController(6, 1, 0.025);
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.InputManager;
-import frc.robot.RobotContainer;
-import frc.robot.Constants.Constants.SwerveConstants;
-
-public class AutoDrive extends Command{
-     public AutoDrive(){
+    public AutoDrive() {
         addRequirements(SWERVE);
+        limelightPID.enableContinuousInput(-Math.PI, Math.PI);
     }
-
-    PIDController limelightPID = new PIDController(0.2, 0.01, 0.0025);
 
     @Override
     public void initialize() {
@@ -31,17 +24,17 @@ public class AutoDrive extends Command{
 
     @Override
     public void execute() {
-		double rotationalVelocity = limelightPID.calculate(SWERVE.getPose().getRotation().getDegrees(), LIMELIGHT_INTERFACE.getSpeakerAngle().getDegrees());
+        double rotationalVelocity = limelightPID.calculate(SWERVE.getPose().getRotation().getRadians(), LIMELIGHT_INTERFACE.getSpeakerAngle().getRadians());
         SWERVE.drive(0, 0, rotationalVelocity);
-        SmartDashboard.putNumber("Drive error", limelightPID.getPositionError());
-       
+        Logger.recordOutput("Drive error", limelightPID.getPositionError());
+
     }
 
     @Override
     public boolean isFinished() {
-         return (abs(limelightPID.getPositionError()) < 1);
+        return (abs(Units.radiansToDegrees(limelightPID.getPositionError())) < 3);
     }
 
-   
+
 }
 
